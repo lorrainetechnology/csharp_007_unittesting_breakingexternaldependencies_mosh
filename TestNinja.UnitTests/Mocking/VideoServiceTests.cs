3530a319a3a698ac0cf9c36ebc258178d0897b17
lorrainetechnology <lorrainetechnology@gmail.com>
2022-05-20 13:25:48 +0530

    breaking external dependencies

---
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class VideoServiceTests
+    {
+        [Test]
+        public void ReadVideoFile_EmptyFile_ReturnErrorMessage()
+        {
+            var service = new VideoService(new FakeFileReader());
+            var result = service.ReadVideoTitle();
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
